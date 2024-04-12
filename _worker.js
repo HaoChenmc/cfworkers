a04fd346f76218bb5e0d95e06eb8c2583fb0b68f
HaoChen <69593279+HaoChenmc@users.noreply.github.com>
2024-04-12 23:01:41 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,6 +2,7 @@ export default {
   async fetch(request, env) {
     const _url = new URL(request.url);
     _url.hostname = _url.pathname.startsWith("/gh/")
+      ? "grafana.haochenmc.top:5443"
       : "grafana.haochenmc.top:5443";
     const req = new Request(_url, request);
     return fetch(req);
