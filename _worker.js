d6111f0b53547ea0143a9cacd3eb70e3faa75f82
HaoChen <69593279+HaoChenmc@users.noreply.github.com>
2024-04-12 23:00:26 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,8 +2,7 @@ export default {
   async fetch(request, env) {
     const _url = new URL(request.url);
     _url.hostname = _url.pathname.startsWith("/gh/")
-      ? "cdn.jsdelivr.net"
-      : "www.baidu.com";
+      : "grafana.haochenmc.top:5443";
     const req = new Request(_url, request);
     return fetch(req);
   },
